9fd91d0b2b57bae53398de9318d6782411e51538
Mathew Mbugua <mbuguam443@gmail.com>
2026-07-03 14:42:18 +0300

    Fix images: commit media/, serve media in production, copy landImage in seed

--- land_selling/urls.py
+++ land_selling/urls.py
@@ -20,6 +20,5 @@ urlpatterns = [
     path('subscriptions/', include('apps.subscriptions.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
